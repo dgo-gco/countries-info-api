e3aeb2e75c89936e5b46b1e8748f00284a7e026e
Diego <110729317+dgo-gco@users.noreply.github.com>
2023-01-31 00:04:23 +0100

    :sparkles: adding functional router showing info

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,39 +4,56 @@ import './Page.css'
 import { useParams } from 'react-router-dom'
 
 export const Page = () => {
-
     const { id } = useParams()
-    console.log(id)
+    // console.log(id)
     const [countryInfo, setCountryInfo] = useState([])
+    // const [countryLanguage, setCountryLanguage] = useState([])
     console.log(countryInfo)
+
    useEffect(() => {
      fetch(`https://restcountries.com/v3.1/alpha/${id}`)
     .then(res => res.json())
     .then(data => setCountryInfo(data))
     }, [])
 
+    // const langs = countryInfo[0].currencies
+    // console.log(langs)
+    // let langues = ''
 
+    // for (let key in langs){
+    //     langues += langs[key] + ', '
+    // }
+
+    // console.log(langues)
+    
   return (
     <div>
         <Navbar />
         <section className="page-container">
-            <div className="country-info-section">
+                {
+                    countryInfo.map((country) => (
+                <div className="country-info-section">
+                <div className='coat-of-arms' style={
+                    {backgroundImage: `url(${country.coatOfArms.png})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPosition: 'center', 
+                    }}></div>
                 <div className="image-flag">
-                    <img src='https://source.unsplash.com/random' alt="" />
-                </div>
+                    <img src={country.flags.png} alt="" />
+                </div>        
                 <div className="country-text">
-                    <h1>HOLA</h1>
-                    <div className="details-country">
-                        <p>Population: </p>
-                        <p>Native name:</p>
-                        <p>Currency:</p>
+                    <h1>{country.name.common}</h1>
+                    <div className="details-country"> 
+                        <p><b>Official name:</b> {country.name.official}</p> 
+                        <p><b>Region:</b> {country.region}</p>
+                        <p><b>Subregion:</b> {country.subregion}</p>
                         <br />
-                        <p>Capital: </p>
-                        <p>Native name:</p>
-                        <p>Currency:</p>
+                        <p><b>Currency:</b> {country.region}</p>
+                        <p><b>Languages:</b> {}</p>
+                        <p><b>Top Level Domain:</b> {country.region}</p>
                     </div>
                 </div>
             </div>
+                    ))   
+                }
         </section>
     </div>
   )
